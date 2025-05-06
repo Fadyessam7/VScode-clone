0a1d0c5e93d33b015322dac505b8366632319f3e
Fadyessam7 <fadyessam17@gmail.com>
2025-05-06 19:54:05 +0300

    refactor render file icon

--- src/components/RenderFileIcon.tsx
+++ src/components/RenderFileIcon.tsx
@@ -1,3 +1,4 @@
+import { extensionIconPaths } from "../constants";
 import IconImg from "./IconImg";
 import FileIcon from "./SVG/File";
 
@@ -6,54 +7,29 @@ interface IProps {
   isFolder?: boolean;
   isOpen?: boolean;
 }
+
 const RenderFileIcon = ({ fileName, isFolder, isOpen }: IProps) => {
   const extension = fileName.split(".").pop();
   //** FILES
-  if (extension === "tsx")
-    return <IconImg src={"/public/icons/react_ts.svg"} />;
-  if (extension === "html") return <IconImg src={"/public/icons/html.svg"} />;
-  if (extension === "svg") return <IconImg src={"/public/icons/svg.svg"} />;
-  if (extension === "js")
-    return <IconImg src={"/public/icons/javascript.svg"} />;
-  if (extension === "ts")
-    return <IconImg src={"/public/icons/typescript.svg"} />;
+  if (
+    extension &&
+    Object.prototype.hasOwnProperty.call(extensionIconPaths, extension)
+  ) {
+    const iconPath = isFolder
+      ? isOpen
+        ? `${extensionIconPaths[extension]}-open.svg`
+        : `${extensionIconPaths[extension]}.svg`
+      : `${extensionIconPaths[extension]}.svg`;
 
-  //** FOLDERS
-  if (extension === "node_modules" && isFolder)
-    return isOpen ? (
-      <IconImg src="/public/icons/folder-node-open.svg" />
-    ) : (
-      <IconImg src="/public/icons/folder-node.svg" />
-    );
-  if (extension === "public" && isFolder)
-    return isOpen ? (
-      <IconImg src="/public/icons/folder-public-open.svg" />
-    ) : (
-      <IconImg src="/public/icons/folder-public.svg" />
-    );
-  if (extension === "src" && isFolder)
-    return isOpen ? (
-      <IconImg src="/public/icons/folder-src-open.svg" />
-    ) : (
-      <IconImg src="/public/icons/folder-src.svg" />
-    );
-  if (extension === "components" && isFolder)
-    return isOpen ? (
-      <IconImg src="/public/icons/folder-components-open.svg" />
-    ) : (
-      <IconImg src="/public/icons/folder-components.svg" />
-    );
-  if (extension === "data" && isFolder)
-    return isOpen ? (
-      <IconImg src="/public/icons/folder-database.svg" />
-    ) : (
-      <IconImg src="/public/icons/folder-database-open.svg" />
-    );
+    return <IconImg src={iconPath} />;
+  }
+  //** Default Folder Icon
   if (isFolder && isOpen)
     return <IconImg src="/public/icons/folder-default-open.svg" />;
   if (isFolder && !isOpen)
     return <IconImg src="/public/icons/folder-default.svg" />;
 
+  //** Default File Icon
   return <FileIcon />;
 };
 
---
+++ src/constants/index.ts
@@ -0,0 +1,15 @@
+export const extensionIconPaths: Record<string, string> = {
+  //** FILES
+  tsx: "/public/icons/react_ts",
+  html: "/public/icons/html",
+  svg: "/public/icons/svg",
+  js: "/public/icons/javascript",
+  ts: "/public/icons/typescript",
+
+  //** FOLDERS
+  node_modules: "/public/icons/folder-node",
+  public: "/public/icons/folder-public",
+  src: "/public/icons/folder-src",
+  components: "/public/icons/folder-components",
+  data: "/public/icons/folder-database",
+};
